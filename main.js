804b826ca180fd703d1b2e7cd746956940f7b3eb
RaFaOViEiRa <146725634+RaFaOViEiRa@users.noreply.github.com>
2024-06-11 08:33:18 -0300

    main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoes - documentos.querySelectorAll(".botao");
+
+for(let 1-0;1 <botoes.length;1++){
+    botoes[1].onclick - function(){
+        
+        for(let j-0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+}
